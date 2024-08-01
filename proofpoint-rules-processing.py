20762217baded21319c5d90b621e029e2d1a1812
somebody <81702427+sOm38Ody@users.noreply.github.com>
2024-08-01 18:23:47 -0400

    moved final regex editing to new function, fixed format bug with OR statement, fixed regex conversion so space after detection word is optional

--- proofpoint-rules-processing.py
+++ proofpoint-rules-processing.py
@@ -6,12 +6,12 @@ import shlex
 def main():
     # example syntax criteria
     # example 1
-    input_string = '( efficacy || quality || sophistication || transparency || criteria || integrity ) FB~5 ( ( rating* NPB ( fitch || draft ) ) || outcome || result || level || score || assessment || decision || analysis ) FB~5 ( not || "didn\'t" || "didn t" || didnt || "wasn\'t" || "wasn t" || wasnt || "isn\'t" || isnt || "isn t" ) FB~5 ( my || our || their || the || we ) FB~5 ( expect* || require* || anticipat* )'
+    #input_string = '( efficacy || quality || sophistication || transparency || criteria || integrity ) FB~5 ( ( rating* NPB ( fitch || draft ) ) || outcome || result || level || score || assessment || decision || analysis ) FB~5 ( not || "didn\'t" || "didn t" || didnt || "wasn\'t" || "wasn t" || wasnt || "isn\'t" || isnt || "isn t" ) FB~5 ( my || our || their || the || we ) FB~5 ( expect* || require* || anticipat* )'
     # example 2 
-    #input_string = '((rating||ratings)NPB (fitch || draft || london ) ) FB~5 ( has || contain* || bunch || filled || riddled || have || all|| full||include* || sent || publish* || issued || release* ) FB~5 ( inaccuracies || inaccurate || ( errors NPB ( human) ) || ( mistakes NPB ( "subject  to" ) ) || miscalculations || oversights )'
+    #input_string = '((rating||ratings)NPB (fitch | | draft || london ) ) FB~5 ( has || contain* || bunch || filled || riddled || have || all|| full||include* || sent || publish* || issued || release* ) FB~5 ( inaccuracies || inaccurate || ( errors NPB ( human) ) || ( mistakes NPB ( "subject  to" ) ) || miscalculations || oversights )'
 
     # user input
-    #input_string = input("Input syntax criteria: ")
+    input_string = input("Input syntax criteria: ")
 
     formatted_input = format(input_string)
     print(f"Corrected Format: {formatted_input}")
@@ -19,29 +19,28 @@ def main():
     input_array = shlex.split(formatted_input)
     print(f"\nSplit: {input_array}\n")
 
-    postfix_stack = toPostFix(input_array)
+    postfix_stack = to_postfix(input_array)
     print(f"Postfix Stack: {postfix_stack}\n")
 
-    # print final answer
-    final_regex = str(eval(postfix_stack))
-    final_regex = final_regex.replace("\\brating|ratings\\b", "\\b(rating(s)?)\\b")
-    final_regex = final_regex.replace("fitch", "(F|f)itch")
+    regex = eval(postfix_stack)
+    final_regex = touch_up(regex)
     print(f"Final Regex:\n{final_regex}")
 
 def format(text):
     text = text.replace("||", "|")
+    text = text.replace("| |", "|")
 
     # surround parenthesis and OR statements with spaces if not already 
-    pattern = re.compile(r'(?<!\s)([()\|]])|(?!\s)([()\|])')
+    pattern = re.compile(r'(?<!\s)([()\|])|(?!\s)([()\|])')
     spaced_text = pattern.sub(lambda m: f" {m.group(0)} ", text)
 
-    # remove duplicate spaces created by replacement
+    # remove duplicate spaces outside of quotes created by replacement
     spaced_text = re.sub(r'\s+(?=([^"]*"[^"]*")*[^"]*$)', " ", spaced_text).strip()
 
     return spaced_text
 
 # convert criteria using shunting yard algorithm
-def toPostFix(array):
+def to_postfix(array):
     operator_stack = []
     output_stack = []
 
@@ -98,33 +97,33 @@ def eval(postfix_stack):
             
             elif "NFB" in element:
                 if "~" in element:
-                    stack.append(f"({left})(?!(\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[4])-1}}})\\b({right})\\b)")
+                    stack.append(f"({left})(?!(\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[4])-1}}})\\b({right})\\b)")
                 else:
-                    stack.append(f"({left})(?!(\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}})\\b({right})\\b)")
+                    stack.append(f"({left})(?!(\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}})\\b({right})\\b)")
 
             elif "FB" in element:
                 if "~" in element:
-                    stack.append(f"\\b({left})\\b\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[3])-1}}}({right})")
+                    stack.append(f"\\b({left})\\b\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[3])-1}}}({right})")
                 else:
-                    stack.append(f"(\\b({left})\\b\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}{right})")
+                    stack.append(f"(\\b({left})\\b\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}{right})")
 
             elif "NPB" in element:
                 if "~" in element:
-                    stack.append(f"(?<!(({right})\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[4])-1}}}))\\b({left})\\b")
+                    stack.append(f"(?<!(({right})\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[4])-1}}}))\\b({left})\\b")
                 else:
-                    stack.append(f"(?<!(({right})\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}))\\b{left}\\b")
+                    stack.append(f"(?<!(({right})\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}))\\b{left}\\b")
 
             elif "PB" in element:
                 if "~" in element:
-                    stack.append(f"(?<=(({right})\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[3])-1}}}))\\b({left})\\b")
+                    stack.append(f"(?<=(({right})\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[3])-1}}}))\\b({left})\\b")
                 else:
-                    stack.append(f"(?<=(({right})\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}))\\b({left})\\b")
+                    stack.append(f"(?<=(({right})\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}))\\b({left})\\b")
 
             elif "NEAR" in element:
                 if "~" in element:
-                    stack.append(f"\\b({left})\\b\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[5])-1}}}\\b({right})\\b|\\b({right})\\b\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[5])-1}}}\\b({left})\\b")
+                    stack.append(f"\\b({left})\\b\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[5])-1}}}\\b({right})\\b|\\b({right})\\b\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,{int(element[5])-1}}}\\b({left})\\b")
                 else:
-                    stack.append(f"\\b({left})\\b\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}\\b({right})\\b|\\b({right})\\b\s+(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}\\b({left})\\b")
+                    stack.append(f"\\b({left})\\b\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}\\b({right})\\b|\\b({right})\\b\s*(?:[\w!\"#$%&\\'()*+,-./:;<=>?@\\\[\]\^_`{{\|}}~]+\s+){{0,0}}\\b({left})\\b")
         
         # element is not an operator
         else:
@@ -136,6 +135,11 @@ def eval(postfix_stack):
     
     # return final regex
     return stack.pop()
+
+def touch_up(regex):
+    regex = regex.replace("\\brating|ratings\\b", "\\b(rating(s)?)\\b")
+    regex = regex.replace("fitch", "(F|f)itch")
+    return regex
 
 if __name__ == "__main__":
     print("================")
